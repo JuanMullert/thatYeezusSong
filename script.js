b844036cb7a311b74173fd4c3000f9289f4226cc
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-05-19 21:27:55 -0500

    created a object, seems more effective

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let fantasySongs = ["Dark Fantasy", "Gorgeous", "Power", "All of the Lights", "Monster", "So Appalled", "Devil in a New Dress", "Runaway", "Hell of a Life", "Blame Game", "Lost in the World"];
-let yeezusSongs = ["On Sight", "I Am a God", "Black Skinhead", "New Slaves"];
-let pabloSongs = ["Ultralight Beam", "Father Stretch My Hands pt.1", "Pt.2", "Famous", "Feedback"];
-let yeSongs = ["I Thought About Killing You", "Yikes", "All Mine", "No Mistakes", "Wouldn't Leave", "Ghost Town", "Violent Crimes"];
-let jikSongs = ["Every Hour", "Selah", "Follow God", "Closed On Sunday", "On God", "Everything We Need", "Water", "God Is", "Hands On", "Use This Gospel", "Jesus is Lord"];
-let dondaSongs = ["Donda Chant", "Jail", "God Breathed", "Off the Grid", "Hurricane", "Praise God", "Jonah", "Ok Ok", "Junya", "Believe What I Say", "24", "Moon", "Remote Control", "Heaven and Hell", "Keep My Spirit Alive", "Jesus Lord"];
-let albumsTogether = fantasySongs.concat(yeezusSongs, pabloSongs, yeSongs, jikSongs, dondaSongs);
-
-let enterSong = prompt('enter song');
-
-    
     // for (let i = 0; i < fantasySongs.length; i++) {
     //     if (inputBox === fantasySongs[i]) {
     //         alert('good song');
--- script2.js
+++ script2.js
@@ -6,23 +6,55 @@ let jikSongs = ["Every Hour", "Selah", "Follow God", "Closed On Sunday", "On God
 let dondaSongs = ["Donda Chant", "Jail", "God Breathed", "Off the Grid", "Hurricane", "Praise God", "Jonah", "Ok Ok", "Junya", "Believe What I Say", "24", "Moon", "Remote Control", "Heaven and Hell", "Keep My Spirit Alive", "Jesus Lord"];
 let albumsTogether = fantasySongs.concat(yeezusSongs, pabloSongs, yeSongs, jikSongs, dondaSongs);
 
+const yeezusInfo = {
+
+    Artist: "Kanye West",
+    Album: "Yeezus",
+    Year: 2013,
+    Songs: ['On Sight',
+        'Black Skinhead',
+        'I Am A God',
+        'New Slaves',
+        'Hold My Liquor',
+        'Blood On The Leaves',
+        'Guilt Trip',
+        'Im In It',
+        'Send It Up',
+        'Bound 2']
+}
+
+
 function songSearch() {
     try {
-        
-        let enterSong = prompt("enter a song");
+        let targetInputField = document.getElementById("inputSong");
+        let inputValue = targetInputField.value;
+
         let details = document.createElement("p");
-        let textDetails = document.createTextNode("good song");
+        details.setAttribute("class", "details-2");
+        let textDetails = document.createTextNode("Year: " + yeezusInfo.Year);
         details.appendChild(textDetails);
+
+
+        let details2 = document.createElement("p");
+        details2.setAttribute("class", "details-2")
+        let textDetails2 = document.createTextNode("Album: " + yeezusInfo.Album);
+        details2.append(textDetails2);
+
+
         let targetDiv = document.getElementById("dropDetails");
         let foundSong = false;
-        for (let i = 0; i < albumsTogether.length; i++) {
-            if (enterSong !== null && (enterSong === albumsTogether[i] || enterSong.toLowerCase() === albumsTogether[i].toLowerCase())) {
+        for (let i = 0; i < yeezusInfo.Songs.length; i++) {
+            if (inputValue !== null && (inputValue === yeezusInfo.Songs[i] || inputValue.toLowerCase() === yeezusInfo.Songs[i].toLowerCase())) {
                 foundSong = true;
                 targetDiv.parentNode.insertBefore(details, targetDiv.nextSibling);
+                targetDiv.parentNode.insertBefore(details2, targetDiv.nextSibling);
                 break;
-                
+
+            } else if (inputValue.length === 0) {
+                alert("please fill the field ")
+                return false;
             }
-        } 
+        }
         if (!foundSong) {
             alert("Song not found")
         }
@@ -33,7 +65,10 @@ function songSearch() {
     }
 }
 
-btn1.onclick = songSearch;
+btn1.onclick = songSearch; // function is called when button is clicked.
+
+console.log(yeezusInfo)
+
 
 
 
